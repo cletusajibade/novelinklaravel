5a9ff9fc5e10957dbf0b1ea190fbbc276036e41d
Cletus Jay Ajibade <cletusajibade@gmail.com>
2024-12-30 15:17:20 -0600

    added migrations for payments and appointments tables

--- database/migrations/2024_12_30_100501_create_consultations_table.php
+++ database/migrations/2024_12_30_100501_create_consultations_table.php
@@ -13,11 +13,32 @@ return new class extends Migration
     {
         Schema::create('consultations', function (Blueprint $table) {
             $table->id();
-            $table->string('u_id');
-            $table->string('first_name');
-            $table->string('last_name');
-            $table->string('email')->unique();
-            $table->string('phone');
+            $table->uuid('uuid')->unique();
+            $table->string('first_name', length: 32);
+            $table->string('last_name', length: 32);
+            $table->string('email', length: 64)->unique();
+            $table->string('phone', length: 32);
+            $table->date('date_of_birth');
+            $table->string('country', length: 64);
+            $table->string('country_of_residence', length: 64);
+            $table->string('country_residence_status', length: 24)->nullable();
+            $table->string('marital_status', length: 16)->nullable();
+            $table->boolean('have_a_passport')->nullable();
+            $table->date('passport_expiry_date')->nullable();
+            $table->string('passport_country', length: 64)->nullable();
+            $table->boolean('family_coming_to_canada')->nullable();
+            $table->string('highest_education', length: 48)->nullable();
+            $table->string('occupation', length: 64)->nullable();
+            $table->integer('years_of_experience')->nullable();
+            $table->boolean('canadian_work_experience')->nullable();
+            $table->boolean('canadian_education')->nullable();
+            $table->boolean('refused_visa')->nullable();
+            $table->boolean('involved_in_genocide')->nullable();
+            $table->boolean('criminal_offence')->nullable();
+            $table->text('background_check_details')->nullable();
+            $table->text('other_information')->nullable();
+            $table->json('consultation_package')->nullable();
+            $table->boolean('active')->default(true);
             $table->timestamps();
         });
     }
---
+++ database/migrations/2024_12_30_195727_create_payments_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('payments', function (Blueprint $table) {
+            $table->id();
+            $table->uuid('uuid')->unique(); //links to Consultation table
+            $table->string('payment_id')->unique();
+            $table->decimal('amount', 15, 2);
+            $table->string('currency', 3);
+            $table->string('status');
+            $table->string('stripe_customer_id')->nullable();
+            $table->string('payment_method_id')->nullable();
+            $table->string('payment_method_type')->nullable();
+            $table->string('card_brand')->nullable();
+            $table->string('card_last4')->nullable();
+            $table->string('description')->nullable();
+            $table->string('refund_status')->nullable();
+            $table->decimal('refund_amount', 15, 2)->nullable();
+            $table->string('dispute_status')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('payments');
+    }
+};
