ba2fb665b912674eacd91e5f78c0f95570ecb253
Cletus Jay Ajibade <cletusajibade@gmail.com>
2025-01-07 17:15:52 -0600

    updated dashboard ui

--- resources/views/dashboard/dashboard.blade.php
+++ resources/views/dashboard/dashboard.blade.php
@@ -8,22 +8,22 @@
 
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white overflow-hidden shadow-lg sm:rounded-lg mb-7">
-                <div class="grid grid-cols-3 gap-5 mt-7 mb-7 mr-3 ml-3">
-                    <x-bladewind::statistic icon_position="right" number="{{ $consultationsCount }}"
-                        label="Total Clients">
+            <div class="overflow-hidden sm:rounded-lg mb-7">
+                <div class="grid grid-cols-3 gap-5 mb-7">
+                    <x-bladewind::statistic icon_position="right" number="{{ $consultationsCount }}" label="Total Clients" class="border-gray-200">
                         <x-slot name="icon">
                             <x-bladewind::icon name="users"
                                 class="!h-14 !w-14 !text-white bg-blue-400 p-4 rounded-full cursor-pointer hover:bg-blue-500 hover:p-3" />
                         </x-slot>
                     </x-bladewind::statistic>
-                    <x-bladewind::statistic icon_position="right" number="{{$appointmentsCount}}" label="Total Appointments">
+                    <x-bladewind::statistic icon_position="right" number="{{ $appointmentsCount }}"
+                        label="Total Appointments" class="border-gray-200">
                         <x-slot name="icon">
                             <x-bladewind::icon name="calendar-days"
                                 class="!h-14 !w-14 !text-white bg-pink-400 p-4 rounded-full cursor-pointer hover:bg-pink-500 hover:p-3" />
                         </x-slot>
                     </x-bladewind::statistic>
-                    <x-bladewind::statistic icon_position="right" number="{{$totalPayments}}" label="Total payments">
+                    <x-bladewind::statistic icon_position="right" number="{{ $totalPayments }}" label="Total payments" class="border-gray-200">
                         <x-slot name="icon">
                             <x-bladewind::icon name="currency-dollar"
                                 class="!h-14 !w-14 !text-white bg-orange-400 p-4 rounded-full cursor-pointer hover:bg-orange-500 hover:p-3" />
