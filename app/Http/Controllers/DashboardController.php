ca89c6674f045c5491d0c2d0e580784d3f5e77dd
Cletus Jay Ajibade <cletusajibade@gmail.com>
2025-01-08 00:17:34 -0600

    tested larapex-charts

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -2,9 +2,13 @@
 
 namespace App\Http\Controllers;
 
+use ArielMejiaDev\LarapexCharts\PieChart;
+use ArielMejiaDev\LarapexCharts\AreaChart;
+use ArielMejiaDev\LarapexCharts\BarChart;
 use App\Models\Appointment;
 use App\Models\Consultation;
 use App\Models\Payment;
+use Carbon\Carbon;
 use Illuminate\Http\Request;
 
 class DashboardController extends Controller
@@ -18,6 +22,45 @@ class DashboardController extends Controller
         $appointmentsCount = Appointment::count() ?? 0;
         $totalPayments = Payment::sum('amount') ?? 0;
         $totalPayments = '$' . number_format($totalPayments, 2);
-        return view('dashboard.dashboard', compact('consultationsCount', 'appointmentsCount', 'totalPayments'));
+
+        $topFiveDates = Payment::orderBy('created_at', 'desc')
+            ->take(5)
+            ->pluck('created_at')
+            ->map(function ($date) {
+                return Carbon::parse($date)->format('Y-m-d');
+            })
+            ->toArray();
+
+        $topFiveAmounts = Payment::orderBy('created_at', 'desc')
+            ->take(5)
+            ->pluck('amount')
+            ->toArray();
+
+        // Pie Chart
+        $chart =  new PieChart();
+        $chart->setTitle('Top 3 scorers of the team.');
+        $chart->setSubtitle('Season 2021.');
+        $chart->addData([20, 24, 30]);
+        $chart->setLabels(['Player 7', 'Player 10', 'Player 9']);
+
+        // Area Chart
+        $area_chart = new AreaChart();
+        $area_chart->setTitle('Sales during 2021.');
+        $area_chart->setSubtitle('Physical sales vs Digital sales.');
+        $area_chart->addData('Physical sales', [40, 93, 35, 42, 18, 82]);
+        $area_chart->addData('Digital sales', [70, 29, 77, 28, 55, 45]);
+        $area_chart->setXAxis(['January', 'February', 'March', 'April', 'May', 'June']);
+
+
+        // Bar Chart
+        $bar_chart = new BarChart();
+        $bar_chart->setTitle('Recent Payments');
+        $bar_chart->setSubtitle('Five most recent payments');
+        $bar_chart->addData('Payment', $topFiveAmounts);
+        // $bar_chart->addData('Boston', [7, 3, 8, 2, 6, 4]);
+        $bar_chart->setXAxis($topFiveDates);
+
+
+        return view('dashboard.dashboard', compact('consultationsCount', 'appointmentsCount', 'totalPayments', 'chart', 'area_chart', 'bar_chart'));
     }
 }
--- resources/views/dashboard/dashboard.blade.php
+++ resources/views/dashboard/dashboard.blade.php
@@ -9,8 +9,16 @@
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="overflow-hidden sm:rounded-lg mb-7">
-                <div class="grid grid-cols-3 gap-5 mb-7">
-                    <x-bladewind::statistic icon_position="right" number="{{ $consultationsCount }}" label="Total Clients" class="border-gray-200">
+                <div class="grid grid-cols-1 md:grid-cols-4 gap-5 mb-7">
+                    <x-bladewind::statistic icon_position="right" number="{{ $consultationsCount }}" label="Total Clients"
+                        class="border-gray-200">
+                        <x-slot name="icon">
+                            <x-bladewind::icon name="users"
+                                class="!h-14 !w-14 !text-white bg-lime-500 p-4 rounded-full cursor-pointer hover:bg-lime-600 hover:p-3" />
+                        </x-slot>
+                    </x-bladewind::statistic>
+                    <x-bladewind::statistic icon_position="right" number="{{ $consultationsCount }}"
+                        label="Client Countries" class="border-gray-200">
                         <x-slot name="icon">
                             <x-bladewind::icon name="users"
                                 class="!h-14 !w-14 !text-white bg-blue-400 p-4 rounded-full cursor-pointer hover:bg-blue-500 hover:p-3" />
@@ -23,7 +31,8 @@
                                 class="!h-14 !w-14 !text-white bg-pink-400 p-4 rounded-full cursor-pointer hover:bg-pink-500 hover:p-3" />
                         </x-slot>
                     </x-bladewind::statistic>
-                    <x-bladewind::statistic icon_position="right" number="{{ $totalPayments }}" label="Total payments" class="border-gray-200">
+                    <x-bladewind::statistic icon_position="right" number="{{ $totalPayments }}" label="Total payments"
+                        class="border-gray-200">
                         <x-slot name="icon">
                             <x-bladewind::icon name="currency-dollar"
                                 class="!h-14 !w-14 !text-white bg-orange-400 p-4 rounded-full cursor-pointer hover:bg-orange-500 hover:p-3" />
@@ -31,9 +40,24 @@
                     </x-bladewind::statistic>
                 </div>
             </div>
-            <div class="bg-white overflow-hidden shadow-lg sm:rounded-lg">
-                charts
+            <div class="grid grid-cols-1 md:grid-cols-2 gap-4">
+                <div class="p-6 bg-white rounded shadow-lg">
+                    {!! $chart->container() !!}
+                </div>
+                 <div class="p-6 bg-white rounded shadow-xl">
+                    {!! $bar_chart->container() !!}
+                </div>
+                <div class="p-6 bg-white rounded shadow-xl">
+                    {!! $area_chart->container() !!}
+                </div>
+
             </div>
         </div>
     </div>
+
+    <script src="{{ $chart->cdn() }}"></script>
+    {{ $chart->script() }}
+    {{ $area_chart->script() }}
+    {{ $bar_chart->script() }}
+
 </x-app-layout>
